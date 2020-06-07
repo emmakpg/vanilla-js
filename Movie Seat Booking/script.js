895ba7937f323c669d32fdd928894c0df8630cd2
Emmanuel Kpogo <emmkpg@gmail.com>
2020-06-08 00:51:45 +0100

    new commit

--- Movie Seat Booking/script.js
+++ Movie Seat Booking/script.js
@@ -1,4 +1,33 @@
 const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row.seat:not(.occupied)");
-const count = document.querySelector("count");
-const total = document.querySelector("total");
+const count = document.querySelector(".count");
+const total = document.querySelector(".price");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = +movieSelect.value; //prse a string to an int by prefixing '+'
+console.log(ticketPrice);
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//Movie select event
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value;
+  updateSelectedCount();
+});
+
+//Seat click event
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
