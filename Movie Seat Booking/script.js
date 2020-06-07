239949bf861fdee4cd217f0c3bb050558b738c99
Emmanuel Kpogo <emmkpg@gmail.com>
2020-06-07 23:03:27 +0100

    new commit

---
+++ Movie Seat Booking/script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row.seat:not(.occupied)");
+const count = document.querySelector("count");
+const total = document.querySelector("total");
