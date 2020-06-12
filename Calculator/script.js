dc143ba26a3c342aed75a3f47e1fb07f2f48d786
Emmanuel Kpogo <emmkpg@gmail.com>
2020-06-12 22:57:51 +0100

    update

--- Calculator/script.js
+++ Calculator/script.js
@@ -10,42 +10,83 @@ function backspace() {
     temp = temp.substring(0, temp.length - 1);
     display.value = temp;
   }
+  fontChange();
+}
+
+function fontChange() {
+  if (display.value.length > 6) {
+    display.style.fontSize = "40px";
+  } else {
+    display.style.fontSize = "60px";
+  }
 }
 
 disp = (num) => {
   switch (num) {
     case 1:
-      display.value += "1";
+      if (display.value.length < 12) {
+        display.value += "1";
+      }
+      fontChange();
       break;
     case 2:
-      display.value += "2";
+      if (display.value.length < 12) {
+        display.value += "2";
+      }
+      fontChange();
       break;
     case 3:
-      display.value += "3";
+      if (display.value.length < 12) {
+        display.value += "3";
+      }
       break;
     case 4:
-      display.value += "4";
+      if (display.value.length < 12) {
+        display.value += "4";
+      }
+      fontChange();
       break;
     case 5:
-      display.value += "5";
+      if (display.value.length < 12) {
+        display.value += "5";
+      }
+      fontChange();
       break;
     case 6:
-      display.value += "6";
+      if (display.value.length < 12) {
+        display.value += "6";
+      }
+      fontChange();
       break;
     case 7:
-      display.value += "7";
+      if (display.value.length < 12) {
+        display.value += "7";
+      }
+      fontChange();
       break;
     case 8:
-      display.value += "8";
+      if (display.value.length < 12) {
+        display.value += "8";
+      }
+      fontChange();
       break;
     case 9:
-      display.value += "9";
+      if (display.value.length < 12) {
+        display.value += "9";
+      }
+      fontChange();
       break;
     case 0:
-      display.value += "0";
+      if (display.value.length < 12) {
+        display.value += "0";
+      }
+      fontChange();
       break;
     case ".":
-      display.value += ".";
+      if (display.value.length < 12) {
+        display.value += ".";
+      }
+      fontChange();
       break;
   }
 };
@@ -54,6 +95,9 @@ operand = (operator) => {
   switch (operator) {
     case "+":
       display.value += "+";
+      break;
+    case "-":
+      display.value += "-";
       break;
     case "/":
       display.value += "/";
@@ -65,4 +109,9 @@ operand = (operator) => {
       display.value += "-" + display.value;
       break;
   }
+};
+
+equal = () => {
+  display.value = eval(display.value);
+  fontChange();
 };
